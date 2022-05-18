41ff3e945c0a199a8479f01cad6b7a345c5fdc01
Jacob Handelman <jacobhandelman@gmail.com>
2022-05-18 11:11:26 -0400

    added api + key (Im aware of vulnerablities from exposing the key, but it is an entirely free key so I'm not too worried. -- added a function to call the weather api (openweathermap) which takes a city as an argument and gets the weather. Currently the status of the weather pertaining to cloud rain or sunshine etc is added to the dom as a string.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './styles.scss';
-console.log('true');
+
+const conditions = document.getElementById('conditions');
+
+const getWeather = async (location) => {
+  try {
+    const fetchWeather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=5a0666d038ba8f944ba08c0c14ce62df`, { mode: 'cors' });
+    const obtainedWeather = await fetchWeather.json();
+
+    conditions.innerText = obtainedWeather.weather[0].main;
+  } catch (err) {
+    alert(err)
+    //make this become (cant find that location)
+  }
+};
+
+getWeather('quebec');
