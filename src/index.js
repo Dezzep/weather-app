a6eac8fea900d396101c2f98ccc453777997cd09
Jacob Handelman <jacobhandelman@gmail.com>
2022-05-19 16:36:27 -0400

    created a jsonProcessor that converts necessary items into a new object so that my code looks cleaner.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-import formListener from './forms';
 import './styles.scss';
-import { convertKelvToCelcAndRoundDown, convertKelvToFarAndRoundDown } from './temp-convert';
-
-let celsius = true;
-const button = document.getElementById('changeTemp');
-const temperature = document.getElementById('temperature');
-const location = document.getElementById('location');
-const conditions = document.getElementById('conditions');
-const weatherIcon = document.getElementById('weather-icon');
-let tempFarenheit;
-let tempCelsius;
+import formListener from './forms';
+import JsonDataFromOpenWeatherMapThatIsUsed from './jsonProcessor';
 
 const weatherForecast5Days = async (cityCode) => {
   if (cityCode !== undefined) {
     const fetchForecast = await fetch('http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=5a0666d038ba8f944ba08c0c14ce62df', { mode: 'cors' });
     const obtainedForecast = await fetchForecast.json();
-    console.log(obtainedForecast);
   }
 };
 
@@ -23,36 +13,14 @@ const getWeather = async (enteredLocation) => {
   try {
     const fetchWeather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${enteredLocation}&APPID=5a0666d038ba8f944ba08c0c14ce62df`, { mode: 'cors' });
     const obtainedWeather = await fetchWeather.json();
+    // goes to a seperate module which takes jsonData needed and updates it to the dom.
+    const obtainTheData = new JsonDataFromOpenWeatherMapThatIsUsed(obtainedWeather);
     console.log(obtainedWeather);
     weatherForecast5Days(obtainedWeather.id);
-    tempCelsius = convertKelvToCelcAndRoundDown(obtainedWeather.main.temp);
-    tempFarenheit = convertKelvToFarAndRoundDown(obtainedWeather.main.temp);
-    conditions.innerText = obtainedWeather.weather[0].description;
-    location.innerText = obtainedWeather.name;
-    weatherIcon.src = `http://openweathermap.org/img/w/${obtainedWeather.weather[0].icon}.png`;
-    if (celsius) {
-      temperature.innerHTML = convertKelvToCelcAndRoundDown(obtainedWeather.main.temp);
-    } else {
-      temperature.innerHTML = convertKelvToFarAndRoundDown(obtainedWeather.main.temp);
-    }
   } catch (err) {
-    alert(err) //change this to --> display unfound city
+    alert(err);
   }
 };
 
 getWeather('montreal');
-const changeTemp = () => {
-  button.addEventListener('click', () => {
-    if (!celsius) {
-      temperature.innerHTML = tempCelsius;
-      button.innerText = 'Change To Farenheit';
-      celsius = true;
-    } else {
-      temperature.innerHTML = tempFarenheit;
-      button.innerText = 'Change To Celsius';
-      celsius = false;
-    }
-  });
-};
-changeTemp();
 formListener(getWeather);
--- src/temp-convert.js
+++ src/temp-convert.js
@@ -1,19 +1,19 @@
-const convertKelvToCelcAndRoundDown = (temp) => {
+const convertKelvToCelcAndRound = (temp) => {
   const kelv = parseInt(temp, 10);
   const cels = kelv - 273.15;
-  const x = Math.floor(cels);
+  const x = Math.round(cels);
   let ans = x.toString();
   ans += '&#x2103';
   return ans;
 };
 
-const convertKelvToFarAndRoundDown = (temp) => {
+const convertKelvToFarAndRound = (temp) => {
   const kelv = parseInt(temp, 10);
   const far = (kelv - 273.15) * (9 / 5) + 32;
-  const x = Math.floor(far);
+  const x = Math.round(far);
   let ans = x.toString();
   ans += '&#8457';
   return ans;
 };
 
-export { convertKelvToCelcAndRoundDown, convertKelvToFarAndRoundDown };
+export { convertKelvToCelcAndRound, convertKelvToFarAndRound };
